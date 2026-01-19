a6510afdc71031ba85896fe786edcdd9c6f6ce3a
HStrangethings <hurwitzho815@gmail.com>
2026-01-19 11:54:37 +0800

    k

---
+++ Assets/Boss System Scripts/Pheonix/PhoenixBoss.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class PhoenixBoss : BossBehaviour
+{
+    public GameObject singleSlash;
+    public GameObject crossSlash;
+    public GameObject laserBeam;
+    public override void Start()
+    {
+        base.Start();
+        //add all the bosses states
+        //pass in the statemachine and this boss instance to the state
+        sm.AddState(new PhoenixIdle(sm, this));
+
+        //add all the bosses attacks
+        mm.AddMove("EnergySlash", new EnergySlash(this, 50, 0));
+        mm.AddMove("TripleSlash", new TripleSlash(this, 50, 0));
+        mm.AddMove("CrossEnergySlash", new EnergySlash(this, 50, 1));
+        mm.AddMove("TripleCrossEnergySlash", new TripleSlash(this, 50, 1));
+        mm.AddMove("LaserBeam", new LaserBeam(this, 3f)); 
+        //mm.AddMove("posMelee", new PosMeleeAttack(this)); //default wave xSize is 9.5
+        //mm.AddMove("boatShield", new BoatShield(this, 50)); //default wave xSize is 9.5
+
+        //start the statemachine with its first state
+        sm.Initialize<PhoenixIdle>();
+
+       // hitboxManager.SetGroupMoveMachines(Harms, mm);
+    }
+}
---
+++ Assets/Boss System Scripts/Pheonix/PhoenixIdle.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class PhoenixIdle : BossState
+{
+    public PhoenixIdle(BossStateMachine sm, BossBehaviour boss) : base(sm, boss) { }
+    private float timer = 0;
+
+    BossStats bossStat;
+
+    public override void Enter()
+    {
+
+    }
+
+    public override void Execute()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            boss.mm.PlayMove("LaserBeam");
+        }
+        //if (needMove)
+        //{
+        //    Vector3 chasePlayerVel = boss.MoveToPlayer();
+        //    boss.rb.linearVelocity = chasePlayerVel;
+        //}
+       boss.rb.linearVelocity = Vector3.zero;
+        Quaternion rotateToPlayer = boss.RotateToPlayer();
+        boss.transform.rotation = rotateToPlayer;
+    }
+
+    public override void Exit()
+    {
+        //Debug.Log("Exitting Poseidon Idle State");
+    }
+}
